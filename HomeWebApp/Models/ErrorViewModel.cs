28ab007502498af200732364504ff93196705eec
Robert Lewis <rlewis@relias.com>
2019-07-24 13:41:02 -0400

    Add project files.

---
+++ HomeWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
